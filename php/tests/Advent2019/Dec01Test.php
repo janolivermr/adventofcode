7a13a7fbdf88f6ea13b61effbaa97d1546edf7ad
Jan Stolle <j.stolle@criteo.com>
2019-12-06 22:49:38 +0100

    Update base test case and refactor Dec01

--- php/tests/Advent2019/Dec01Test.php
+++ php/tests/Advent2019/Dec01Test.php
@@ -4,9 +4,9 @@ namespace Tests\Advent2019;
 
 use AoC\Advent2019\Dec01\PartOne;
 use AoC\Advent2019\Dec01\PartTwo;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCaseWithInput;
 
-class Dec01Test extends TestCase
+class Dec01Test extends TestCaseWithInput
 {
     /**
      * @dataProvider partOneExamplesProvider
@@ -41,5 +41,23 @@ class Dec01Test extends TestCase
             [1969, 966],
             [100756, 50346],
         ];
+    }
+
+    public function runPartOne()
+    {
+        $fuelRequired = 0;
+        foreach ($this->getInput() as $record) {
+            $fuelRequired += PartOne::calculateFuel((int)$record[0]);
+        }
+        return $fuelRequired;
+    }
+
+    public function runPartTwo()
+    {
+        $fuelRequired = 0;
+        foreach ($this->getInput() as $record) {
+            $fuelRequired += PartTwo::calculateFuelRecursive((int)$record[0]);
+        }
+        return $fuelRequired;
     }
 }
--- php/tests/TestCaseWithInput.php
+++ php/tests/TestCaseWithInput.php
@@ -10,29 +10,56 @@ use PHPUnit\Framework\TestCase;
 
 abstract class TestCaseWithInput extends TestCase
 {
-    protected function getInput(): Iterator
+    protected string $year;
+    protected string $day;
+    protected string $path;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
     {
+        parent::__construct($name, $data, $dataName);
         $reverse_path = preg_replace(
             '/^Tests\\\\(.+)\\\\(\w+)Test$/',
-            '$1'.DIRECTORY_SEPARATOR.'$2.txt',
+            '$1|$2',
             static::class
         );
+        list($year, $day) = explode('|', $reverse_path);
         $pathComponents = [
             __DIR__,
             '..',
             '..',
             'input',
+            $year,
+            $day,
         ];
-        $path = implode(DIRECTORY_SEPARATOR, array_merge($pathComponents, explode('\\', $reverse_path)));
+        $path = implode(DIRECTORY_SEPARATOR, $pathComponents).'.txt';
+        $this->year = $year;
+        $this->day = $day;
+        $this->path = $path;
+    }
+
+    protected function getInput(): Iterator
+    {
         try {
-            $reader = Reader::createFromPath($path);
+            $reader = Reader::createFromPath($this->path);
             return $reader->getRecords();
         } catch (Exception $e) {
             return new EmptyIterator();
         }
     }
 
-    abstract public function testPartOne();
+    abstract public function runPartOne();
 
-    abstract public function testPartTwo();
+    abstract public function runPartTwo();
+
+    public function testPartOne()
+    {
+        $this->assertTrue(true);
+        echo sprintf("Result for %s, %s\n%s", $this->year, $this->day, $this->runPartOne());
+    }
+
+    public function testPartTwo()
+    {
+        $this->assertTrue(true);
+        echo sprintf("Result for %s, %s\n%s", $this->year, $this->day, $this->runPartTwo());
+    }
 }
